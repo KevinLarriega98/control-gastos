4f6765eda4f4438e36f094f5225034721f81ba33
KevinLarriega98 <kevinlarriega@gmail.com>
2024-02-15 12:10:06 +0100

    added new expense button and expense modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react"
 import Header from "./components/Header"
+import Modal from "./components/Modal"
+import NuevoGastoSVG from './img/nuevo-gasto.svg'
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0)
   const [esUnPresupuestoValido, setEsUnPresupuestoValido] = useState(false)
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+
+  const handleNuevoGasto = () => {
+    setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500)
+  }
 
   return (
     <div>
@@ -13,6 +25,15 @@ function App() {
         esUnPresupuestoValido={esUnPresupuestoValido}
         setEsUnPresupuestoValido={setEsUnPresupuestoValido}
       />
+
+      {esUnPresupuestoValido && (
+        <div className="nuevo-gasto">
+          <img src={NuevoGastoSVG} alt="Nuevo Gasto" onClick={handleNuevoGasto} />
+        </div>
+      )}
+
+      {modal && <Modal setModal={setModal} animarModal={animarModal} setAnimarModal={setAnimarModal} />}
+
     </div>
   )
 }
